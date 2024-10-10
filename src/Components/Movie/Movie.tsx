db203ca6656c8b10fbd1cb277867248be0aafc8b
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-10 18:07:49 +0600

    Added typification

--- src/Components/Movie/Movie.tsx
+++ src/Components/Movie/Movie.tsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from "react";
-import { IMovie } from "../../types";
+import React, { ChangeEvent, useEffect } from 'react';
+import { IMovie } from '../../types';
 
 interface IMovieProps extends IMovie {
   updateMovie: (id: string, updatedName: string) => void;
@@ -7,7 +7,7 @@ interface IMovieProps extends IMovie {
 }
 
 const Movie: React.FC<IMovieProps> = React.memo(
-  ({ name, id, updateMovie, deleteMovie }) => {
+  ({name, id, updateMovie, deleteMovie}) => {
     useEffect((): void => {
       console.log(`[Movie] ${name} render`);
     }, [name]);
@@ -18,7 +18,7 @@ const Movie: React.FC<IMovieProps> = React.memo(
           type="text"
           value={name}
           key={id}
-          onChange={(e) => updateMovie(id, e.target.value)}
+          onChange={(e: ChangeEvent<HTMLInputElement>) => updateMovie(id, e.target.value)}
           className="form-control w-100"
         />
         <button
@@ -29,7 +29,7 @@ const Movie: React.FC<IMovieProps> = React.memo(
       </div>
     );
   },
-  (prevProps, nextProps) => {
+  (prevProps: Readonly<IMovieProps>, nextProps: Readonly<IMovieProps>) => {
     return prevProps.name === nextProps.name && prevProps.id === nextProps.id;
   },
 );
