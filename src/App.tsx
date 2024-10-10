27449ad6297823d5e45cb69ef3c42788eb5e48e2
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-10 13:45:47 +0600

    Displayed 2 containers and added logic to toggle between them

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,22 @@
 import "./App.css";
+import { useState } from "react";
+import Movies from "./Containers/Movies/Movies.tsx";
+import Jokes from "./Containers/Jokes/Jokes.tsx";
 
 const App = () => {
-  return <></>;
+  const [page, setPage] = useState<boolean>(false);
+  return (
+    <div className="container m-4">
+      <button
+        type="button"
+        className="btn btn-dark"
+        onClick={() => setPage(!page)}
+      >
+        Change page
+      </button>
+      {page ? <Jokes /> : <Movies />}
+    </div>
+  );
 };
 
 export default App;
