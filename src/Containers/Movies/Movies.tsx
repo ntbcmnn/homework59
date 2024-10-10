da7d662905aa3ed2d4d5e8370be74dab1f1c8887
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-10 14:13:20 +0600

    1. Created an array of movies; 2. Created 3 methods: add, update, delete to change the state of movies array 3. Displayed 2 components: MoviesForm & Movie 4. Added a validation to check if the movies array is empty

---
+++ src/Containers/Movies/Movies.tsx
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import MoviesForm from "../../Components/MoviesForm/MoviesForm.tsx";
+import Movie from "../../Components/Movie/Movie.tsx";
+import { IMovie } from "../../types";
+
+const Movies = () => {
+  const [movies, setMovies] = useState<IMovie[]>([
+    {
+      name: "Harry Potter",
+      id: "1",
+    },
+
+    {
+      name: "Truman show",
+      id: "2",
+    },
+
+    {
+      name: "Forrest Gump",
+      id: "3",
+    },
+  ]);
+
+  const addMovie = (movie: IMovie) => {
+    setMovies((prevState) => [
+      ...prevState,
+      {
+        name: movie.name,
+        id: String(new Date()),
+      },
+    ]);
+  };
+
+  const updateMovie = (id: string, updatedName: string): void => {
+    setMovies((prevState) =>
+      prevState.map(movie =>
+        movie.id === id ? { ...movie, name: updatedName } : movie,
+      ),
+    );
+  };
+
+  const deleteMovie = (id: string): void => {
+    setMovies((prevState) => prevState.filter((movie) => movie.id !== id));
+  };
+
+  return (
+    <div>
+      <MoviesForm addMovie={addMovie} />
+      {movies.length === 0 ? (
+        <h4 className="text-dark-emphasis">No movies have been added yet.</h4>
+      ) : (
+        movies.map((movie) => (
+          <Movie
+            key={movie.id}
+            id={movie.id}
+            name={movie.name}
+            updateMovie={updateMovie}
+            deleteMovie={deleteMovie}
+          />
+        ))
+      )}
+    </div>
+  );
+};
+
+export default Movies;
