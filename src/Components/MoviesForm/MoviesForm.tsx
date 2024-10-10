39d52fc723cbdb096e531bed04e752ea37087f0e
Natalya Botsman <botsmannatashaa@gmail.com>
2024-10-10 14:22:49 +0600

    1. Created method that handles the submit event: it prevents page reload and adds a new movie to array 2. Displayed form with its methods

---
+++ src/Components/MoviesForm/MoviesForm.tsx
@@ -0,0 +1,41 @@
+import React, { FormEvent, useState } from "react";
+import { IMovie } from "../../types";
+
+interface Props {
+  addMovie: (movie: IMovie) => void;
+}
+
+const MoviesForm: React.FC<Props> = ({ addMovie }) => {
+  const [newMovieName, setNewMovieName] = useState<string>("");
+
+  const handleSubmit = (e: FormEvent<HTMLFormElement>): void => {
+    e.preventDefault();
+
+    if (newMovieName.trim().length > 0) {
+      addMovie({ id: String(new Date()), name: newMovieName });
+      setNewMovieName("");
+    } else {
+      alert("Enter movie name!");
+    }
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className="d-flex justify-content-start gap-3 my-4"
+    >
+      <input
+        type="text"
+        value={newMovieName}
+        placeholder="Movie name..."
+        onChange={(e) => setNewMovieName(e.target.value)}
+        className="form-control w-25"
+      />
+      <button type="submit" className="btn btn-primary">
+        Add to list
+      </button>
+    </form>
+  );
+};
+
+export default MoviesForm;
